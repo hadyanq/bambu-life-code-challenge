bb8ad8cde43e2d479995fa2e4c8a497b3d616d3c
Hadyan Qashidi <hadyan.qashidi@gmail.com>
2018-11-06 09:07:27 +0800

    Added floor, ceiling and interval

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,83 @@
 <template>
-  <div>
-    <h1>Bambu.Life Code Challenge</h1>
+  <div class="main-wrapper">
+    <price-chart 
+      :ceiling="highestPrice"
+      :floor="lowestPrice"
+      :interval="filteredByMonth"
+    />
   </div>
 </template>
+
+<script>
+  import priceData from "./priceData.json"
+  import PriceChart from "./components/PriceChart.vue"
+
+  export default {
+    components: { PriceChart },
+    data() {
+      return {
+        priceData: priceData["Time Series (Daily)"],
+        month: "09",
+        stock: "MSFT"
+      }
+    },
+    computed: {
+      filteredByMonth() {
+        let filtered = {}
+        for (let d in this.priceData) {
+          const removeDays = d.substring(0, 7)
+          if (removeDays === `2018-${this.month}`) {
+            filtered[d] = this.priceData[d]
+          }
+        }
+
+        return filtered
+      },
+      highestPrice() {
+        const days = Object.keys(this.filteredByMonth)
+        let currentD  = days[0]
+        let ceiling = { [currentD]: this.filteredByMonth[currentD] }
+
+        for (let d in this.filteredByMonth) {
+          if (
+            Number(this.filteredByMonth[d]["2. high"]) > 
+            Number(ceiling[currentD]["2. high"])
+          ) {
+            currentD = d
+            ceiling = { [currentD]: this.filteredByMonth[currentD] }
+          }
+        }
+
+        return ceiling
+      },
+      lowestPrice() {        
+        const days = Object.keys(this.filteredByMonth)
+        let currentD  = days[0]
+        let floor = { [currentD]: this.filteredByMonth[currentD] }
+
+        for (let d in this.filteredByMonth) {
+          if (
+            Number(this.filteredByMonth[d]["3. low"]) < 
+            Number(floor[currentD]["3. low"])
+          ) {
+            currentD = d
+            floor = { [currentD]: this.filteredByMonth[currentD] }
+          }
+        }
+
+        return floor
+      }
+    }
+  }
+</script>
+
+<style>
+  .main-wrapper {
+    width: 860px;
+    margin: auto;
+  }
+
+  .chart-wrapper {
+    background-color: lightsteelblue;
+  }  
+</style>
---
+++ src/components/PriceChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg class="chart-wrapper" width="860px" height="420px">
+    <line x1="30" x2="30" y1="30" y2="140" stroke="green" stroke-width="3"/>
+    
+    <line x1="20" x2="30" y1="50" y2="50" stroke="green" stroke-width="3"/>
+    <line x1="30" x2="40" y1="120" y2="120" stroke="green" stroke-width="3"/>
+  </svg>
+</template>
+
+<script>
+  export default {
+    props: {
+      ceiling: {
+        type: Object,
+        default: () => {}
+      },
+      floor: {
+        type: Object,
+        default: () => {}
+      },
+      interval: {
+        type: Object,
+        default: () => {}
+      }
+    }
+  }
+</script>
